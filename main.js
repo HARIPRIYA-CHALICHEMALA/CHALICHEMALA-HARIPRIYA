f3c65b6c49a0b0174e1cb07fa6d1ba9eafbc3b27
CHALICHEMALA HARIPRIYA <haripriyacsse>
2018-04-04 10:46:34 +0530

    data.json

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 function loadJSON(file,callback){
-  var xhr=new  XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr .onreadystatechange=function(){
-    if(xhr.readyState===4 && xhr.status=="200"){
-      callback(xhr.responseText);
-    }
-  };
-  xhr.send();
+var xhr=new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.open("GET",file,true);
+xhr.onreadystatechange=function(){
+  if(xhr.readyState ===4 && xhr.status== "200"){
+    callback(xhr.responseText);
+  }
+};
+xhr.send();
 }
 loadJSON("data.json",function(text){
-  let data=JSON.parse(text);
-  console.log(data);
-  Career(data.Career);
+let data=JSON.parse(text);
+console.log(data);
+career(data.career);
+education(data.education);
 })
-var right=document.querySelector(".content");
-function Career( car){
-  console.log(car.info);
-  var h3=document.createElement("h3");
-  h3.textContent="Career Objective";
-  right.appendChild(h3);
-  var hr=document.createElement("hr");
-  right.appendChild(hr);
-  var p=document.createElement("p");
-  p.textContent=car.info;
-  right.appendChild(p);
+var right=document.querySelector (".Content");
+function Career(car) {
+ console.log(car.info);
+ var h3=document.createElement("h3");
+ h3.textContent="Career Objective";
+ right.appendChild(h3);
+ var hr=document.createElement("hr");
+ right.appendChild(hr);
+ var p=document.createElement("p");
+ p.textContent=car.info;
+ right.appendChild(p);
+
+}
+function education(edu) {
+ var h3=document.createElement("h3");
+ h3.textContent="EDUCATION";
+ right.appendChild(h3);
+ var hr=document.createElement("hr");
+ right.appendChild(hr);
+
+ var ul=document.createElement("ul");
+ right.appendChild(ul);
+ for(var i=edu.length-1;i>=0;i++){
+   console.log(edu[i].degree);
+ }
 }
