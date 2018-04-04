6b237a9bfd4b59571445eaaf1ccd0cd5a02f1fd5
CHALICHEMALA HARIPRIYA <haripriyacsse>
2018-04-04 12:27:57 +0530

    files updated

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ xhr.send();
 loadJSON("data.json",function(text){
 let data=JSON.parse(text);
 console.log(data);
-career(data.career);
+career(data.Career);
 education(data.education);
+skills(data.skills);
 })
-var right=document.querySelector (".Content");
-function Career(car) {
+var right=document.querySelector (".content");
+function career(car) {
  console.log(car.info);
  var h3=document.createElement("h3");
  h3.textContent="Career Objective";
@@ -37,7 +38,37 @@ function education(edu) {
 
  var ul=document.createElement("ul");
  right.appendChild(ul);
- for(var i=edu.length-1;i>=0;i++){
+ for(var i=edu.length-1;i>=0;i--){
    console.log(edu[i].degree);
+   var li=document.createElement("li");
+   li.textContent=edu[i].degree;
+     ul.appendChild(li);
+
+     var p=document.createElement("p");
+     p.textContent=edu[i].school;
+     li.appendChild(p);
+
+     var ul1=document.createElement("ul");
+     ul.appendChild(ul1);
+     for(j=0;j<edu[i].data.length;j++){
+       var li1=document.createElement("li");
+       li1.textContent=edu[i].data;
+       ul1.appendChild(li1);
+     }
  }
+}
+function skills(skill){
+  var h3=document.createElement("h3");
+  h3.textContent="skills";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var table=document.createElement("table");
+  table.border="1";
+  right.appendChild(table);
+  let row="";
+  for(var i=0;i<skill.length;i++){
+    row+="<tr><td>"+skill[i].name+"</td><td>"+skill[i].info+"</td></tr>";
+  }
+  table.innerHTML=row;
 }
